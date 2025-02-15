81408df5a26cf10c06df2101643872b2fb0e386a
michalswig <mike@data.pl>
2025-02-15 17:46:05 +0100

    app init

---
+++ src/test/java/com/mike/order_management_system/OrderManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mike.order_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
